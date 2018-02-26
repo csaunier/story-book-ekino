30f94584995759816b3249ad2541e31d01e3734c
Aude Planchamp <aude.planchamp@ekino.com>
2018-02-26 11:30:29 +0100

    feat(storybook): add some stories with title

--- src/components/title/title.js
+++ src/components/title/title.js
@@ -3,7 +3,12 @@ import PropTypes from 'prop-types'
 
 import styles from './title.scss'
 
-const Title = ({ text }) => <h1 className={styles.wrapper}>{text}</h1>
+const Title = ({ text, subtitle }) => (
+  <div className={styles.wrapper}>
+    <h1 className={styles.title}>{text}</h1>
+    {subtitle && <h2 className={styles.subtitle}>{subtitle}</h2>}
+  </div>
+)
 
 Title.propTypes = {
   text: PropTypes.string.isRequired,
--- src/components/title/title.stories.js
+++ src/components/title/title.stories.js
@@ -3,4 +3,7 @@ import { storiesOf } from '@storybook/react'
 
 import Title from './title'
 
-storiesOf('Title', module).add('with text', () => <Title text="je suis un titre" />)
+storiesOf('Title', module).add('simple', () => <Title text="Je suis un titre" />)
+storiesOf('Title', module).add('with subtitle', () => (
+  <Title text="Je suis un titre" subtitle="Je suis un sous-titre" />
+))
