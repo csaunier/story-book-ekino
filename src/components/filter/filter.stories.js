63a860f9326134e9033c1524a66887c20c4d89e5
Aude Planchamp <aude.planchamp@ekino.com>
2018-03-16 20:50:00 +0100

    feat(stories): add knobs and info to filter story

--- src/components/filter/filter.stories.js
+++ src/components/filter/filter.stories.js
@@ -1,20 +1,35 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
-import { linkTo } from '@storybook/addon-links'
-// import { LinkTo } from '@storybook/addon-links/react';
+// import { linkTo } from '@storybook/addon-links'; // pas très utile
+import { withInfo } from '@storybook/addon-info'
+import { withKnobs, text } from '@storybook/addon-knobs/react'
 import withTests from '../withTests'
 
 import Filter from './filter'
 
-storiesOf('Filter', module)
+const stories = storiesOf('Filter', module)
+
+stories.addDecorator(withKnobs)
+
+stories
   .add('nothing specific selected', () => (
     <Filter selectedFilter="" selectFilter={action('item selected')} />
   ))
-  .add('selected item', () => <Filter selectedFilter="6" selectFilter={action('item selected')} />)
+  .add('selected item', () => {
+    const selected = text('filter', '')
+    return <Filter selectedFilter={selected} selectFilter={action('item selected')} />
+  })
+  .add(
+    'simple info',
+    withInfo(`
+      This is a filter with no specific value selected
+    
+      ~~~js
+       <Filter selectedFilter="" selectFilter={action('item selected')} />
+      ~~~
+    
+    `)(() => <Filter selectedFilter="" selectFilter={action('item selected')} />),
+  )
   .addDecorator(withTests('filter'))
   .add('This story shows test results from Filter', () => <div>Jest results in storybook</div>)
-
-storiesOf('Filter with link addon', module).add('selected item', () => (
-  <Filter selectedFilter="" selectFilter={linkTo('Filter', { selectedFilter: 'senior' })} />
-))
