3fca2078f7f84d35ec62107a0fbda72344ac6d9b
Cédric Saunier <cedric.saunier@fullsix.com>
2018-03-20 14:21:19 +0100

    feat(storybook): add accessiblity addon

--- src/components/tronche/tronche.stories.js
+++ src/components/tronche/tronche.stories.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { withKnobs, text } from '@storybook/addon-knobs/react'
+import { checkA11y } from '@storybook/addon-a11y'
+import { withInfo } from '@storybook/addon-info'
 
 import team from '../../../mocks/team.json'
 
@@ -8,18 +10,41 @@ import Tronche from './tronche'
 
 const stories = storiesOf('Tronche', module)
 
+const gender = text('gender', team.team[0].gender)
+const firstName = text('firstName', team.team[0].firstName.toLowerCase())
+const name = text('name', team.team[0].name.toLowerCase())
+
 stories.addDecorator(withKnobs)
+stories.addDecorator(checkA11y)
 
-stories.add('simple', () => {
-  const gender = text('gender', team.team[0].gender)
-  const firstName = text('firstName', team.team[0].firstName.toLowerCase())
-  const name = text('name', team.team[0].name.toLowerCase())
+stories.add('simple', () => (
+  <Tronche
+    defaultSrc="/assets/img/team/default.jpg"
+    src={`/assets/img/team/${firstName}-${name}.jpg`}
+    gender={gender}
+  />
+))
 
-  return (
+const stories2 = storiesOf('Tronche', module)
+
+stories2.add(
+  'with info',
+  withInfo(`
+      This is a filter with no specific value selected
+
+      ~~~js
+       <Tronche
+        defaultSrc="/assets/img/team/default.jpg"
+        src={\`/assets/img/team/${firstName}-${name}.jpg\`}
+        gender={gender}
+      />
+      ~~~
+
+    `)(() => (
     <Tronche
       defaultSrc="/assets/img/team/default.jpg"
       src={`/assets/img/team/${firstName}-${name}.jpg`}
       gender={gender}
     />
-  )
-})
+  )),
+)
