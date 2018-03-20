b09c743d563315b1031d1692338dc109cb4d9341
Aude Planchamp <aude.planchamp@ekino.com>
2018-03-20 13:59:34 +0100

    feat(storybook): clean some files and add options addon

--- src/components/filter/filter.stories.js
+++ src/components/filter/filter.stories.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
-// import { linkTo } from '@storybook/addon-links'; // pas très utile
-import { withInfo } from '@storybook/addon-info'
-import { withKnobs, text } from '@storybook/addon-knobs/react'
+import { withKnobs } from '@storybook/addon-knobs/react'
 import withTests from '../withTests'
 
 import Filter from './filter'
@@ -16,20 +14,5 @@ stories
   .add('nothing specific selected', () => (
     <Filter selectedFilter="" selectFilter={action('item selected')} />
   ))
-  .add('selected item', () => {
-    const selected = text('filter', '')
-    return <Filter selectedFilter={selected} selectFilter={action('item selected')} />
-  })
-  .add(
-    'simple info',
-    withInfo(`
-      This is a filter with no specific value selected
-    
-      ~~~js
-       <Filter selectedFilter="" selectFilter={action('item selected')} />
-      ~~~
-    
-    `)(() => <Filter selectedFilter="" selectFilter={action('item selected')} />),
-  )
   .addDecorator(withTests('filter'))
   .add('This story shows test results from Filter', () => <div>Jest results in storybook</div>)
--- src/components/title/title.stories.js
+++ src/components/title/title.stories.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import withTests from '../withTests'
 
 import Title from './title'
 
@@ -8,7 +7,3 @@ storiesOf('Title', module).add('simple', () => <Title text="Je suis un titre" />
 storiesOf('Title', module).add('with subtitle', () => (
   <Title text="Je suis un titre" subtitle="Je suis un sous-titre" />
 ))
-
-storiesOf('Title', module)
-  .addDecorator(withTests('title'))
-  .add('This story shows test results from Title', () => <div>Jest results in storybook</div>)
